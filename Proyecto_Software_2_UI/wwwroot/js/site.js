42a7698c422815f2b6495bd3b8087634f337b118
Wilmer Diaz <amadormido@gmail.com>
2025-04-06 08:47:31 -0600

    site.js: Se revierten cambios para evitar futuros conflictos.

--- Proyecto_Software_2_UI/wwwroot/js/site.js
+++ Proyecto_Software_2_UI/wwwroot/js/site.js
@@ -2,15 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-// When tab is closing
-window.addEventListener('beforeunload', function () {
-    // Delete the session cookies
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i];
-        const eqPos = cookie.indexOf("=");
-        const name = eqPos > -1 ? cookie.substr(0, eqPos).trim() : cookie.trim();
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
-
-});
